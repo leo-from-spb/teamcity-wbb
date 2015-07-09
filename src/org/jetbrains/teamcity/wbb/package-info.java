da10b6022ec4e7a3458e91f123b8aead485d929a
Leonid Bushuev <leonidos@gmail.com>
2015-07-09 14:19:36 +0200

    Package info

---
+++ src/org/jetbrains/teamcity/wbb/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * TeamCity plugin "Who Broke Build"
+ *
+ * <p>
+ *   Project page: <a href="https://github.com/leo-from-spb/teamcity-wbb">teamcity-wbb</a> on GitHub.
+ * </p>
+ *
+ * @author Leonid Bushuev from JetBrains
+ **/
+package org.jetbrains.teamcity.wbb;
